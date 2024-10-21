b708c9175f16a471af7269b6bb5f8b4e8880797e
INDRAJ V <indrajvenkatesh@gmail.com>
2024-10-21 23:27:17 +0530

    app_write_constants.dart

--- lib/constants/app_write_constants.dart
+++ lib/constants/app_write_constants.dart
@@ -1,6 +1,6 @@
 class AppWriteConstants {
-  static const String projectID = '6714c32b002022e52589';
+  static const String projectID = '';
   static const String projectEndPoint = 'http://localhost:80/v1';
-  static const String dataBaseId = '6714d017003b92fd6f11';
-  static const String userCollectionID = '6714d01d001be3c4eed4';
+  static const String dataBaseId = '';
+  static const String userCollectionID = '';
 }
